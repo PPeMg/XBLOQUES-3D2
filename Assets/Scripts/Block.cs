52880b9d192c71fcf511de2f2437317fae69f06a
PPeMg <jdeldo@gmail.com>
2017-07-25 13:38:29 +0200

    Added Explosion Effect to Game

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
     public Points p;
+    public GameObject explosion;
 
     private void OnCollisionEnter(Collision collision) {
+        Instantiate(explosion, this.transform.position, Quaternion.identity);
         Destroy(this.gameObject);
         p.addPoints();
     }
